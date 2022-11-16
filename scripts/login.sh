a21dcddc037e445883d06462a3411cfb4bdd5644
Ben Chacko <bchacko@pivotal.io>
2022-11-16 20:47:36 +0000

    update login script

--- scripts/login.sh
+++ scripts/login.sh
@@ -1,14 +1,7 @@
 #!/bin/bash 
 
-read -p "Enter vSphere password: " pass
-
-
-if [ -z "$pass" ]; then
-  echo "vSphere password is required!"
-else
-  export KUBECTL_VSPHERE_PASSWORD=$pass
+export KUBECTL_VSPHERE_PASSWORD=$pass
 
   kubectl vsphere login --vsphere-username=administrator@vsphere.local --server=<supervisor-server> --insecure-skip-tls-verify --tanzu-kubernetes-cluster-namespace dev_namespace
   kubectl vsphere login --vsphere-username=administrator@vsphere.local --server=<supervisor-server> --insecure-skip-tls-verify --tanzu-kubernetes-cluster-namespace dev_namespace --tanzu-kubernetes-cluster-name services
   kubectl vsphere login --vsphere-username=administrator@vsphere.local --server=<supervisor-server> --insecure-skip-tls-verify --tanzu-kubernetes-cluster-namespace dev_namespace --tanzu-kubernetes-cluster-name workload
-fi
