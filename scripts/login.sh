1684a3e3f2ca06bea681f79b1585bba46dd63b94
Ben Chacko <bchacko@pivotal.io>
2022-11-18 04:20:00 +0000

    update login script

--- scripts/login.sh
+++ scripts/login.sh
@@ -1,7 +1,30 @@
-#!/bin/bash 
+#!/bin/bash
 
-export KUBECTL_VSPHERE_PASSWORD=$pass
+cleanup () {
+ unset  KUBECTL_VSPHERE_PASSWORD
+}
 
-  kubectl vsphere login --vsphere-username=administrator@vsphere.local --server=<supervisor-server> --insecure-skip-tls-verify --tanzu-kubernetes-cluster-namespace dev_namespace
-  kubectl vsphere login --vsphere-username=administrator@vsphere.local --server=<supervisor-server> --insecure-skip-tls-verify --tanzu-kubernetes-cluster-namespace dev_namespace --tanzu-kubernetes-cluster-name services
-  kubectl vsphere login --vsphere-username=administrator@vsphere.local --server=<supervisor-server> --insecure-skip-tls-verify --tanzu-kubernetes-cluster-namespace dev_namespace --tanzu-kubernetes-cluster-name workload
+trap cleanup SIGTERM
+
+# replace with base64 encoded password for vsphere
+KUBECTL_VSPHERE_PASSWORD_B64="changeme"
+
+alias k=kubectl
+alias krr="kubectl rollout restart"
+alias kcg="kubectl config get-contexts"
+alias kcu="kubectl config use-context"
+
+alias kgp="kubectl get pod"
+alias kgs="kubectl get service"
+alias kgd="kubectl get deploy"
+alias kgi="kubectl get ingress"
+alias kgc="kubectl get configmap"
+alias kgn="kubectl get namespace"
+alias kgnode="kubectl get node"
+
+export KUBECTL_VSPHERE_PASSWORD=`echo $KUBECTL_VSPHERE_PASSWORD_B64 | base64 -d`
+kubectl vsphere login --vsphere-username=$user@vsphere.local --server=vc01cl01-wcp.h2o-2-2086.h2o.vmware.com --insecure-skip-tls-verify --tanzu-kubernetes-cluster-namespace h2o --tanzu-kubernetes-cluster-name workload
+
+kubectl vsphere login --vsphere-username=$user@vsphere.local --server=vc01cl01-wcp.h2o-2-2086.h2o.vmware.com --insecure-skip-tls-verify --tanzu-kubernetes-cluster-namespace h2o --tanzu-kubernetes-cluster-name worker
+
+cleanup
